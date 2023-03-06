91d63e144a92137e04abfad1cf3642fc4dcde425
Mikko Leppänen <mikko.leppanen@vaisala.com>
2023-03-06 09:43:10 +0200

    Integration test Minor refactoring/reshaping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use email_newsletter::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
