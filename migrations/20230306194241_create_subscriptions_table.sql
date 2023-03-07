0c3f131af2dd4daab13b407ccb1143563f56ae2b
Mikko Leppänen <mikko.leppanen@vaisala.com>
2023-03-07 17:39:00 +0200

    Persisting subscriber + related tests

--- migrations/20230306194241_create_subscriptions_table.sql
+++ migrations/20230306194241_create_subscriptions_table.sql
@@ -1,8 +1,9 @@
 -- Add migration script here
-CREATE TABLE IF NOT EXISTS subscriptions(
-    id uuid NOT NULL,
+CREATE TABLE subscriptions
+(
+    id            uuid        NOT NULL,
     PRIMARY KEY (id),
-    email TEXT NOT NULL UNIQUE,
-    name TEXT NOT NULL,
+    email         TEXT        NOT NULL UNIQUE,
+    name          TEXT        NOT NULL,
     subscribed_at timestamptz NOT NULL
 );
